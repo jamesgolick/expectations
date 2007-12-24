87b8c7eefb8a222c567b3b21fc7a14091deef041
jaycfields <jaycfields@6f265639-962f-4d85-a13b-3090ac0aa348>
2007-12-24 19:35:06 +0000

    more formatting
    git-svn-id: http://expectations.rubyforge.org/svn/trunk@7 6f265639-962f-4d85-a13b-3090ac0aa348

--- lib/expectations/suite_results.rb
+++ lib/expectations/suite_results.rb
@@ -18,12 +18,16 @@ class Expectations::SuiteResults
       out.puts "\n\nSuccess: #{fulfilled.size} fulfilled"
     else
       out.puts "\n\nFailure: #{failure.size} failed, #{error.size} errors, #{fulfilled.size} fulfilled"
-      out.puts
-      out.puts "Errors:"
+      out.puts "\nErrors:" if error.any?
       error.each do |err|
         out.puts "In #{err.file}:#{err.line}"
         out.puts "  error: <#{err.exception.message}>"
         out.puts "  expected: <#{err.expected.inspect}> got: <#{err.actual.inspect}>"
+      end
+      out.puts "\nFailures:" if failure.any?
+      failure.each do |fail|
+        out.puts "In #{fail.file}:#{fail.line}"
+        out.puts "  expected: <#{fail.expected.inspect}> got: <#{fail.actual.inspect}>"
       end
     end
   end
