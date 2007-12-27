619edf98a82c7baa61219e6e20ac4d36b17e4f75
jaycfields <jaycfields@6f265639-962f-4d85-a13b-3090ac0aa348>
2007-12-27 19:26:27 +0000

    new version to handle small numbers
    git-svn-id: http://expectations.rubyforge.org/svn/trunk@15 6f265639-962f-4d85-a13b-3090ac0aa348

--- rakefile.rb
+++ rakefile.rb
@@ -41,7 +41,7 @@ specification = Gem::Specification.new do |s|
   expect NoMethodError do 
     Object.invalid_method_call 
   end."
-	s.version = "0.0.2"
+	s.version = "0.0.3"
 	s.author = 'Jay Fields'
   s.description = "A lightweight unit testing framework. Tests (expectations) will be written as follows 
   expect 2 do 
@@ -58,7 +58,7 @@ specification = Gem::Specification.new do |s|
   s.extra_rdoc_files = ['README']
   s.rdoc_options << '--title' << 'expectations' << '--main' << 'README' << '--line-numbers'
                          
-  s.autorequire = 'expectations'
+  s.email = 'ruby@jayfields.com'
   s.files = FileList['{lib,test}/**/*.rb', '[A-Z]*$', 'rakefile.rb'].to_a
 	s.test_file = "test/all_tests.rb"
 end
