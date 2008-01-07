d11af8ead85b44c2fb9b10cdc0e0595246c5cff5
jaycfields <jaycfields@6f265639-962f-4d85-a13b-3090ac0aa348>
2008-01-07 05:03:17 +0000

    v 0.0 .8 fixing another bug
    git-svn-id: http://expectations.rubyforge.org/svn/trunk@24 6f265639-962f-4d85-a13b-3090ac0aa348

--- lib/expectations/suite.rb
+++ lib/expectations/suite.rb
@@ -7,12 +7,23 @@ class Expectations::Suite
   def execute(out=STDOUT)
     suite_result = Expectations::SuiteResults.new(out)
     benchmark = Benchmark.measure do
-      expectations.each do |expectation|
-        suite_result << expectation.execute
-      end
+      ENV["LINE"].nil? ? run_all(suite_result) : run_one(suite_result)
     end
     suite_result.print_results(benchmark)
     suite_result.result
+  end
+  
+  def run_all(suite_result)
+    expectations.each do |expectation|
+      suite_result << expectation.execute
+    end
+  end
+  
+  def run_one(suite_result)
+    one = expectations.inject do |result, expectation|
+      expectation.line > ENV["LINE"].to_i ? result : expectation
+    end
+    suite_result << one.execute
   end
 
   def expectations
