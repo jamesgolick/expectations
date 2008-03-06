45cf78c3a86250a2df8259a1ba711b290c2f387e
jaycfields <jaycfields@6f265639-962f-4d85-a13b-3090ac0aa348>
2008-03-06 18:07:32 +0000

    v 2.2
    git-svn-id: http://expectations.rubyforge.org/svn/trunk@53 6f265639-962f-4d85-a13b-3090ac0aa348

--- lib/expectations/recorded_expectation.rb
+++ lib/expectations/recorded_expectation.rb
@@ -1,14 +1,17 @@
-module Expectations::RecordedStateBasedExpectation
+module Expectations::RecordedExpectation
   def execute
     begin
       mocha_setup
-      instance_exec expected.subject, &block
-      if expected.verify
+      instance_exec expected.subject!, &block
+      if expected.verify!
         self.extend(Expectations::Results::Fulfilled)
       else
         self.extend(Expectations::Results::StateBasedFailure)
-        self.message = expected.message
+        self.message = expected.failure_message
       end
+    rescue Mocha::ExpectationError => ex
+      self.extend(Expectations::Results::BehaviorBasedFailure)
+      self.message = expected.mocha_error_message(ex)
     rescue Exception => ex
       self.extend(Expectations::Results::Error)
       self.exception = ex
